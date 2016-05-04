2eb31cabfe0b3b8e720ab6dcdf233fc4581007ee
TimD <timd@siqinc.com>
2016-05-04 14:52:59 -0400

    Single instance, and auto startup added

--- pw/dekk/Application.java
+++ pw/dekk/Application.java
@@ -1,10 +1,13 @@
 package pw.dekk;
 
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
 import org.jnativehook.GlobalScreen;
 import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.NativeKeyEvent;
 import pw.dekk.model.Response;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +16,27 @@ import java.util.List;
  */
 public class Application implements org.jnativehook.keyboard.NativeKeyListener {
 
-   private static List<Response<NativeKeyEvent>> responses;
+    private static List<Response<NativeKeyEvent>> responses;
+    private static boolean AUTO_ENTER = true;
+    private static int SLEEP_TIME = 50;
+    private static final int PORT = 9999;
 
     static {
         responses = new ArrayList<>();
-        responses.add(new F12());
     }
 
     public static void main(String[] args) {
         try {
+            if (Util.running()) {
+                JOptionPane.showConfirmDialog(null, "JINU is alreay running on this system. Exiting", "Instance Conflict", JOptionPane.DEFAULT_OPTION);
+                System.exit(1);
+            }
+            if (args != null && args.length == 2) {
+                AUTO_ENTER = args[0].equals("true");
+                SLEEP_TIME = Integer.parseInt(args[1]);
+                System.out.println(AUTO_ENTER + " " + SLEEP_TIME);
+            }
+            Util.register();
             GlobalScreen.registerNativeHook();
         } catch (NativeHookException ex) {
             System.err.println("There was a problem registering the native hook.");
@@ -29,6 +44,29 @@ public class Application implements org.jnativehook.keyboard.NativeKeyListener {
             System.exit(1);
         }
         GlobalScreen.addNativeKeyListener(new Application());
+        branch();
+    }
+
+    /**
+     * Decides on whether or not to automatically login or wait for F12 key
+     */
+    private static void branch() {
+        if (AUTO_ENTER) {
+            Runnable scan = () -> {
+                while (true) {
+                    WinDef.HWND hwnd = User32.INSTANCE.FindWindow(null, "Login");
+                    if (hwnd != null) {
+                        User32.INSTANCE.ShowWindow(hwnd, 9);        // SW_RESTORE
+                        User32.INSTANCE.SetForegroundWindow(hwnd);   // bring to front
+                        new F12().react(null);
+                    }
+                    Util.sleep(SLEEP_TIME);
+                }
+            };
+            new Thread(scan).start();
+        } else {
+            responses.add(new F12());
+        }
     }
 
     @Override
--- pw/dekk/Util.java
+++ pw/dekk/Util.java
@@ -1,7 +1,12 @@
 package pw.dekk;
 
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.io.*;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
 
 /**
  * Created by TimD on 5/3/2016.
@@ -11,6 +16,25 @@ public class Util {
     public static final int NONE = 0;
     public static final int TAB = 1;
     public static final int ENTER = 2;
+    private static ServerSocket socket;
+
+    static void register() {
+       if(writeStartFile()){
+           System.out.println("Startup file written successfully");
+       }
+    }
+
+    static boolean running() {
+        try {
+            //Bind to localhost adapter with a zero connection queue
+            socket = new ServerSocket(9999, 0, InetAddress.getByAddress(new byte[]{127, 0, 0, 1}));
+        } catch (BindException e) {
+            return true;
+        } catch (IOException e) {
+            return true;
+        }
+        return false;
+    }
 
     static {
         try {
@@ -39,6 +63,38 @@ public class Util {
                 sendKey(KeyEvent.VK_ENTER);
                 return;
         }
+    }
+
+    public static void sleep(int sleepTime) {
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static String locate() {
+        return new File("").getAbsolutePath() + "\\JINU.jar";
+    }
+
+    static boolean writeStartFile() {
+        File file = new File(startDirectory() + "JINU.bat");
+        if(!file.exists()) {
+            try {
+                PrintWriter pw = new PrintWriter(new FileOutputStream(file, false));
+                pw.print("start javaw -jar " + locate() + " true 50 \r\n" +
+                        "exit");
+                pw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String startDirectory() {
+        return System.getProperty("java.io.tmpdir").replace("Local\\Temp\\", "Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\");
     }
 }
 
